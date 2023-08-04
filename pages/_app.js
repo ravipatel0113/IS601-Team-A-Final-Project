195d67cc7a1d4cc0687deddca1500d34dd8c2754
Ravi Patel <rp70960@gmail.com>
2023-08-04 17:41:53 -0400

    process layout to all pages

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,12 @@ import '../styles/global.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
+import Layout from '../components/layout';
 
 export default function App({ Component, pageProps }) {
     return (
+      <Layout>
       <Component {...pageProps} />
+      </Layout>
     );
   }
