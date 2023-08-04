3d930fb07f4f9e6f2a25436aaeef486003611fe0
Ravi Patel <rp70960@gmail.com>
2023-08-03 20:05:05 -0400

    Initial NextJs Setup

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import Head from 'next/head';
+import Image from 'next/image';
+// import mainStyles from '../styles/main.module.css';
+
+import React from 'react';
+
+export default function Home() {
+    return(
+        <>
+        <h1>Hello</h1>
+        </>
+    );
+}
