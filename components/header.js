48901fafb082791067a19c29190d4b321e6dbdbd
Ravi Patel <rp70960@gmail.com>
2023-08-06 23:04:18 -0400

    Update Header Section

--- components/header.js
+++ components/header.js
@@ -6,13 +6,18 @@ export default function Header() {
     <>
         <section className={mainStyles.menu}>
 
+        <div className= {mainStyles.logo}>
+            <img src='/images/logo.png'></img>
+        </div>
         <div className={mainStyles.logoname}>
-            <Link href='/' className={mainStyles.homelink}>Home</Link>
+            
         </div>
 
         <div className={mainStyles.pagelink}>
-
-        <Link href="/posts/about-us" className={mainStyles.links}> About Us </Link>
+            <Link href='/' className={mainStyles.links}>Home</Link>
+            <Link href='/' className={mainStyles.links}>Blog</Link>
+            <Link href="/posts/about-us" className={mainStyles.links}> About Us </Link>
+            <Link href='/' className={mainStyles.links}>Contact Us</Link>
 
         </div>
         </section>
