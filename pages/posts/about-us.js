2073e351abedddf77b551791b93f5498c8c64faa
Ravi Patel <rp70960@gmail.com>
2023-08-06 20:38:46 -0400

    Added Hero Section 2

---
+++ pages/posts/about-us.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function AboutUsPost() {
+    return(
+        <>
+        <Head>
+            <title>About US</title>
+        </Head>
+        <h1>About Us</h1>
+    </>
+    ); 
+}
